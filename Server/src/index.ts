1ce33b5e564404f2758c655e004e4d27cc678808
Ruan De Villiers <ruan.devilliers@nintex.com>
2024-05-10 15:15:34 +0200

    feat: added socket.io

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,18 +1,52 @@
 import express, { Request, Response } from 'express';
 import path from "path";
 import cors from 'cors';
-
+import { Server } from 'socket.io';
+import http from 'http';
+ 
 const app = express();
+app.use(cors);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express Server!');
+// Serve the Socket.IO client library
+app.get('/socket.io/socket.io.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'node_modules', 'socket.io', 'client-dist', 'socket.io.js'));
 });
 
-app.listen(port, () => {
+app.get('/', (req: Request, res: Response) => {
+  res.redirect('/index.html');
+});
+
+// Socket.IO event handling
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  
+    socket.on('chat message', (msg) => {
+      console.log('Message: ' + JSON.stringify(msg));
+      io.emit('chat message', JSON.stringify(msg));
+    });
+  });
+  
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
