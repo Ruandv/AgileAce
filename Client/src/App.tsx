b7b2530ac3c95d51ce8808972bef38214b631139
Ruandv <Ruandv@users.noreply.github.com>
2024-05-10 21:40:45 +0200

    feat: added client code (#3)

---
+++ Client/src/App.tsx
@@ -0,0 +1,21 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NewGame from './components/newRoom/NewRoom';
+import ChatRoom from './components/chatroom/ChatRoom';
+ import './App.css';
+import { SocketProvider } from './contexts/SocketContext';
+const App = () => {
+  return (
+    <SocketProvider url="http://localhost:3000">
+    <Router>
+      <Routes>
+        <Route path="/newRoom" element={<NewGame />} />
+        <Route path="/chat" element={<ChatRoom/>} />
+      </Routes>
+    </Router>
+ </SocketProvider>
+  );
+};
+
+export default App;
---
+++ Client/src/contexts/enums.ts
@@ -0,0 +1,5 @@
+export enum SocketEvents {
+    Connect = "connect",
+    Joined = "joined",
+    Send = "send"
+  }
