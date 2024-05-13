5bf0c5a36ea268b6f012bb31070a329ffe1cf6ac
Ruan De Villiers <ruan.devilliers@nintex.com>
2024-05-13 22:27:50 +0200

    chore: removed provider

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,10 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewGame from './components/newRoom/NewRoom';
 import ChatRoom from './components/chatroom/ChatRoom';
  import './App.css';
-import { SocketProvider } from './contexts/SocketContext';
 const App = () => {
   return (
-    <SocketProvider url={process.env.REACT_APP_API_URL!}>
     <Router>
       <Routes>
         <Route path="/newRoom" element={<NewGame />} />
@@ -15,7 +13,6 @@ const App = () => {
         <Route path="/" element={<NewGame />} />
       </Routes>
     </Router>
- </SocketProvider>
   );
 };
 
