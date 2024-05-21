087e76834b27dad9c643a1932fb594d593d20bf1
ProfessorUltra <wynand.cothill@outlook.com>
2024-05-21 07:57:26 +0200

    fix: poker card sliding active inactive logic

--- Client/src/components/chatroom/ChatRoom.tsx
+++ Client/src/components/chatroom/ChatRoom.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './ChatRoom.css';
-import ChatRoomService from '../../services/chatRoom.service';
+import React, { useEffect, useRef, useState } from "react";
+import "./ChatRoom.css";
+import ChatRoomService from "../../services/chatRoom.service";
 
 function ChatRoom() {
   const message = useRef<HTMLInputElement>(null);
@@ -10,7 +10,7 @@ function ChatRoom() {
     const doWork = async () => {
       const messages = await chatRoomService.getMessages();
       setChat(messages);
-    }
+    };
     doWork();
   }, []);
 
@@ -18,38 +18,61 @@ function ChatRoom() {
     e.preventDefault();
     if (message.current) {
       chatRoomService.send(message.current.value);
-      message.current.value = '';
+      message.current.value = "";
     }
   };
 
   return (
-      <div className="min-w-96">
-        <div className="flex flex-col h-screen screen bg-gray-100 text-sm">
-          <div className="overflow-auto h-full px-6 py-4">
-            <div className="border-b border-gray-300 mb-6 pb-2 flex justify-between items-center">
-              <h1 className="text-3xl font-semibold">Chat Room ({chatRoomService.getUserCount()})</h1>
-            </div>
-            <div className="chat-history space-y-4">
-              {chat.map((msg: any, index) => (
-                <div key={index} className="p-4 rounded-lg bg-white shadow">
-                  <p className="text-gray-800">{msg.text}</p>
-                </div>
-              ))}
-            </div>
+    <div>
+      <div className="flex flex-col h-screen screen bg-gray-100 text-sm">
+        <div className="overflow-auto h-full px-6 py-4">
+          <div className="border-b border-gray-300 mb-6 pb-2 flex justify-between items-center">
+            <h1 className="text-3xl font-semibold">
+              Chat Room ({chatRoomService.getUserCount()})
+            </h1>
           </div>
-          <form className="border-t border-gray-300 p-4 bg-white" onSubmit={submitMessage}>
-            <div className="flex space-x-4">
-              <input
-                id="messageInput"
-                ref={message}
-                className="flex-grow rounded-lg border-gray-300 p-2"
-                placeholder="Type your message"
-              />
-              <button type="submit" className="bg-blue-500 text-white rounded-lg px-4 py-2">Send</button>
-            </div>
-          </form>
+          <div className="chat-history space-y-4">
+            {chat.map((msg: any, index) => (
+              <div key={index} className="p-4 rounded-lg bg-white shadow">
+                <div className="flex">
+                  <div className="w-1/4 m-2">
+                    
+                    <img className="card-img-top" src="https://i.pravatar.cc/200" width="100%" alt="Card image cap"></img>
+                  </div>
+                  <div className="w-3/4 m-2">
+                    <h3 className=" font-bold">John</h3>
+                    <small>2024-05-17 22:05</small>
+                  </div>
+                </div>
+                <div className="m-2">
+                    <p className="text-gray-800">{msg.text}</p>
+                  </div>
+              </div>
+            ))}
+          </div>
         </div>
+        <form
+          className="border-t border-gray-300 p-4 bg-white"
+          onSubmit={submitMessage}
+        >
+          <div className="flex space-x-4 flex-row flex-grow">
+            <input
+              id="messageInput"
+              ref={message}
+              className="flex-grow rounded-lg border-gray-300 p-2 blcok"
+              placeholder="Type your message"
+            />
+            
+          </div>
+          <button
+              type="submit"
+              className="bg-blue-500 text-white rounded-sm px-4 py-2"
+            >
+              Send
+            </button>
+        </form>
       </div>
+    </div>
   );
 }
 
--- Client/src/pages/playRoom/PlayRoom.tsx
+++ Client/src/pages/playRoom/PlayRoom.tsx
@@ -1,34 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import ChatRoom from '../../components/chatroom/ChatRoom';
-import PokerCard from '../../components/pokerCard/PokerCard';
-
+import React, { useEffect, useState } from "react";
+import ChatRoom from "../../components/chatroom/ChatRoom";
+import ChatRoomService from "../../services/chatRoom.service";
+import "./PlayRoom.css";
+import PokerCard from "../../components/pokerCard/PokerCard";
 
 const NewRoom = () => {
-    useEffect(() => {
-        console.log('NewRoom component mounted');
-    }, []);
-    const [selected, setSelected] = useState<number | null>(null);
+  const [playCards, setPlayCards] = useState<number[]>([]);
+  useEffect(() => {
+    const doWork = async () => {
+      setPlayCards(await service.getPlayCards());
+    };
+    doWork();
+  }, []);
+  const [selected, setSelected] = useState<number | null>(null);
+  const service = ChatRoomService.getInstance();
 
-    return (
-        <div className="flex h-screen ">
-            <div id='left'><ChatRoom></ChatRoom></div>
-            <div id='rhs' className='flex flex-col flex-grow'>
-                <div id='toolbar' className="bg-red-500 flex flex-col items-center justify-center min-h-[3em]">toolbar</div>
-                <div id='right' className="bg-red-300 flex flex-col items-center justify-center flex-grow">
-                    <span className='flex flex-row'>
-                        {selected}
-                        {[1, 2, 3, 4].map((i) =>
-                            <PokerCard
-                                display={i.toString()}
-                                isActive={(selected !== null && selected === i) ? true : false}
-                                onClick={() => { setSelected(i) }}
-                            />
-                        )}
-                    </span>
-                </div>
-            </div>
-        </div>
+  const [activeButton, setIsActiveButton] = useState(0);
+
+  function toggleButton(buttonIndex: number): void {
+    const buttons: NodeListOf<HTMLButtonElement> = document.querySelectorAll(
+      '[class^="votebutton"]'
     );
+    const currentButton: HTMLButtonElement = buttons[buttonIndex - 1];
+
+    if (
+      activeButton === buttonIndex &&
+      currentButton.classList.contains("slide-up")
+    ) {
+      // Slide down the button if already in "slide-up" position
+      currentButton.classList.remove("slide-up");
+      setIsActiveButton(0);
+    } else {
+      // Slide down previous button and slide up current button
+      if (activeButton !== 0) {
+        buttons[activeButton - 1].classList.remove("slide-up");
+      }
+      currentButton.classList.add("slide-up");
+      setIsActiveButton(buttonIndex);
+    }
+    console.log("active button = " + activeButton);
+  }
+
+  return (
+    <>
+<nav className="bg-white border-gray-200 dark:bg-gray-900">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Agile Ace</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    {/* <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div> */}
+  </div>
+</nav>
+<div className="mycontainer">
+        <div className="mychat-pane" id="chatroom"><ChatRoom></ChatRoom></div>
+        <div className="myplay-area" id="playroom">
+        <div className="poker-table">
+        <div
+          id="right"
+          className="flex items-center justify-center"
+        >
+          <span className="flex flex-row m-5">
+            {playCards.map((i) => (
+              <PokerCard
+                display={i.toString()}
+                onClick={() => toggleButton(Number(i))}
+              />
+            ))}
+            </span>
+        </div>
+      </div>
+        </div>
+    </div>
+  </>
+  );
 };
 
 export default NewRoom;
